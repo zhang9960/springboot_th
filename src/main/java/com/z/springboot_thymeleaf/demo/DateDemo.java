c9538373ef5a005e7d6add1266aa7fa566940bba
zhang9960 <1137725988@qq.com>
2021-03-19 10:33:46 +0800

    第四次修改demo

--- src/main/java/com/z/springboot_thymeleaf/demo/DateDemo.java
+++ src/main/java/com/z/springboot_thymeleaf/demo/DateDemo.java
@@ -11,6 +11,8 @@ import java.util.Date;
 public class DateDemo {
    //在demo中添加了一行注释
     //第二次在demo中添加注释
+
+    //第四次修改demog
     public static void main(String[] args) {
         Date date = new Date();
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH-mm-ss");
